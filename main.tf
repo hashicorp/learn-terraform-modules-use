86417aab7d42f5363f094f556ef1472d15457143
Tu Nguyen <im2nguyen@gmail.com>
2022-04-26 22:27:43 -0700

    Update module to use latest version, use count meta-argument

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ provider "aws" {
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "2.21.0"
+  version = "3.14.0"
 
   name = var.vpc_name
   cidr = var.vpc_cidr
@@ -26,10 +26,10 @@ module "vpc" {
 
 module "ec2_instances" {
   source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "2.12.0"
+  version = "3.5.0"
+  count   = 2
 
-  name           = "my-ec2-cluster"
-  instance_count = 2
+  name = "my-ec2-cluster"
 
   ami                    = "ami-0c5204531f799e0c6"
   instance_type          = "t2.micro"
