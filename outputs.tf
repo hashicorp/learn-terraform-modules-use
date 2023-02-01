92ea0d35735204de2cba142f9e3c91c7f4c635d2
dlaguerta <dianne@hashicorp.com>
2023-02-01 14:49:05 -0800

    Add copyright headers

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,6 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
 output "vpc_public_subnets" {
   description = "IDs of the VPC's public subnets"
   value       = module.vpc.public_subnets
