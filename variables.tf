173b9bc7d790600153d15982195139164655ef0c
Franz Mejia <mejiafranz@gmail.com>
2023-06-10 22:37:36 -0400

    Add ubuntu ami and change region

--- variables.tf
+++ variables.tf
@@ -16,7 +16,7 @@ variable "vpc_cidr" {
 variable "vpc_azs" {
   description = "Availability zones for VPC"
   type        = list(string)
-  default     = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  default     = ["us-east-2a", "us-east-2b", "us-east-2c"]
 }
 
 variable "vpc_private_subnets" {
